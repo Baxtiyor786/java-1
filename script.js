2af30b4324f2aad824feef263d47928b6549fc02
Baxtiyor786 <jomoliddinovbaxtiyor@gmail.com>
2024-12-14 17:43:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let userName = prompt('Ismingizni kiriting');
+console.log(`Привет, ${userName}! `)
+
+let тв = prompt('Familiyangizni kiriting');
+console.log(`Привет, ${userName}!`)
+
+let us = prompt('Yoshingizni kiriting');
+console.log(` ${userName}!`)
+
+let фs = prompt('Maxalezi kiriting');
+console.log(`Привет, ${userName}!`)
+
+let иs = prompt('Nomeringizni kiriting');
+console.log(`Привет, ${userName}!`)
+
+alert(`kirganiz uchun raxmat ${userName}`)
